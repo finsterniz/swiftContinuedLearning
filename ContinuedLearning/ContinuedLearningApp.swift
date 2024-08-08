7245090b9a24475f4f7a48d1b8bc880936e817ce
boiltaimn <34036969+finsterniz@users.noreply.github.com>
2024-08-08 11:59:48 +0200

    init
    init

---
+++ ContinuedLearning/ContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuedLearningApp.swift
+//  ContinuedLearning
+//
+//  Created by a on 08.08.24.
+//
+
+import SwiftUI
+
+@main
+struct ContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
