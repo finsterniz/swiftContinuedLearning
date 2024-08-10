12a9244941b78b01a7291f761518b88d91981c94
boiltaimn <34036969+finsterniz@users.noreply.github.com>
2024-08-10 18:26:44 +0200

    DragGestureBootcamp2

---
+++ ContinuedLearning/DragGestureBootcamp2.swift
@@ -0,0 +1,83 @@
+//
+//  DragGestureBootcamp2.swift
+//  ContinuedLearning
+//
+//  Created by a on 10.08.24.
+//
+
+import SwiftUI
+
+struct DragGestureBootcamp2: View {
+    @State var startingOffsetY: CGFloat = UIScreen.main.bounds.height * 0.85 // 屏幕尺寸最大高度的一半
+    @State var currentDragOffsetY: CGFloat = 0
+    @State var endingOffsetY: CGFloat = 0
+    
+    var body: some View {
+        ZStack{
+            Color.green.ignoresSafeArea()
+            
+            MySignUpView()
+                .offset(y: startingOffsetY)
+                .offset(y: currentDragOffsetY)
+                .offset(y: endingOffsetY)
+                .gesture(
+                    DragGesture()
+                        .onChanged({ value in
+                            withAnimation(.spring()){
+                                currentDragOffsetY = value.translation.height
+                            }
+                        })
+                        .onEnded({ value in
+                            withAnimation(.spring()){
+                                if currentDragOffsetY < -150{
+                                    endingOffsetY = -startingOffsetY
+                                }else if endingOffsetY != 0 && currentDragOffsetY > 150{
+                                    endingOffsetY = 0
+                                }
+                                currentDragOffsetY = 0
+                            }
+                        })
+                )
+            
+            Text("\(currentDragOffsetY)")
+        }
+        .ignoresSafeArea(edges: .bottom)
+    }
+}
+
+#Preview {
+    DragGestureBootcamp2()
+}
+
+struct MySignUpView: View {
+    var body: some View {
+        VStack(spacing: 20){
+            Image(systemName: "chevron.up")
+                .padding(.top)
+            
+            Text("Sign up")
+                .font(.headline)
+                .fontWeight(.semibold)
+            
+            Image(systemName: "flame.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+            
+            Text("This is the description for our app. This is my favorite SwiftUI course and i recommend to all of my friends to subscribe to Swiftful Thinking!")
+                .multilineTextAlignment(.center)
+            
+            Text("Create an Account")
+                .foregroundStyle(.white)
+                .font(.headline)
+                .padding()
+                .padding(.horizontal)
+                .background(Color.black.cornerRadius(10))
+            
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+    }
+}
